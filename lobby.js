9394d9d957b38d7f39cc98192a57a3a93aa6a766
Thomas Chapeaux <tom@wooclap.com>
2021-03-13 12:47:52 +0100

    ⚙ Move Lobby logic to its own file

---
+++ lobby.js
@@ -0,0 +1,134 @@
+module.exports = class Lobby {
+  constructor(name, hostId) {
+    this.name = name;
+    this.host = hostId;
+    this.lastAccessDate = new Date();
+
+    this.nicknames = {};
+    this.mysteryNames = {};
+    this.ownedBy = {};
+
+    this.locked = false;
+    this.phase1Locked = false;
+    this.displayOrder = null;
+    this.turnOfPlayer = null;
+    this.lastAction = null;
+
+    this.andTheWinnerIs = null;
+  }
+
+  addPlayer(socketId, nickname) {
+    if (!this.locked && !Object.keys(this.nicknames).includes(socketId)) {
+      this.nicknames[socketId] = nickname;
+    }
+
+    if (process.env.DEBUG) {
+      // set last player as host
+      this.host = socketId;
+    }
+  }
+
+  setNickname(socketId, newNickname) {
+    if (!this.locked && Object.keys(this.nicknames).includes(socketId)) {
+      this.nicknames[socketId] = newNickname;
+    }
+  }
+
+  lockAndGo(socketId) {
+    if (socketId === this.host) {
+      this.locked = true;
+    }
+  }
+
+  setMysteryName(socketId, mysteryName) {
+    if (
+      this.locked === true &&
+      !Object.keys(this.mysteryNames).includes(socketId)
+    ) {
+      this.mysteryNames[socketId] = mysteryName;
+
+      if (
+        Object.keys(this.mysteryNames).length ===
+        Object.keys(this.nicknames).length
+      ) {
+        this.prepareGuessingGame();
+      }
+    }
+  }
+
+  prepareGuessingGame() {
+    const playerIds = Object.keys(this.nicknames);
+
+    this.phase1Locked = true;
+    this.displayOrder = shuffleArray(playerIds);
+    this.turnOfPlayer = playerIds[getRandomInt(playerIds.length)];
+
+    if (process.env.DEBUG) {
+      this.turnOfPlayer = this.host;
+    }
+  }
+
+  blame(blamerId, mysteryName, blamedId) {
+    const playerIds = Object.keys(this.nicknames);
+    const mysteryNames = Object.values(this.mysteryNames);
+
+    // Some checks
+
+    // blamer and blamed exist
+    if (!playerIds.includes(blamerId) || !playerIds.includes(blamedId)) {
+      return;
+    }
+
+    // mysteryName exists
+    if (!mysteryNames.includes(mysteryName)) {
+      return;
+    }
+
+    // it is the turn of the blamer
+    if (this.turnOfPlayer !== blamerId) {
+      return;
+    }
+
+    // blamer and blamed are not owned
+    if (
+      Object.keys(this.ownedBy).includes(blamerId) ||
+      Object.keys(this.ownedBy).includes(blamedId)
+    ) {
+      return;
+    }
+
+    // Finally, let's do the blaming
+    const isBlamingCorrect = this.mysteryNames[blamedId] === mysteryName;
+    if (isBlamingCorrect) {
+      this.lastAction = `✅ ${this.nicknames[blamerId]} guessed ${mysteryName} was written by ${this.nicknames[blamedId]}`;
+      this.ownedBy[blamedId] = blamerId;
+    } else {
+      // No owning, but change turn of player
+      this.lastAction = `❌ ${this.nicknames[blamerId]} guessed ${mysteryName} was written by ${this.nicknames[blamedId]}`;
+      this.turnOfPlayer = blamedId;
+    }
+
+    // WIN condition
+    if (
+      Object.keys(this.ownedBy).length ===
+      Object.keys(this.nicknames).length - 1
+    ) {
+      this.turnOfPlayer = null;
+      this.andTheWinnerIs = blamerId;
+    }
+  }
+
+  restart(socketId) {
+    if (socketId !== this.host) {
+      return;
+    }
+
+    this.mysteryNames = {};
+    this.ownedBy = {};
+    this.phase1Locked = false;
+    this.displayOrder = null;
+    this.turnOfPlayer = null;
+
+    this.andTheWinnerIs = null;
+  }
+};
